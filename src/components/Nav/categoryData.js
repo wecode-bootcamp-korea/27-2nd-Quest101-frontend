cf9b2255c56dfb806f8ac461b988f56b6fb9ad46
dnjstd <dnjstd@gmail.com>
2021-12-14 17:11:08 +0900

    Modify: nav category 구현 완료

---
+++ src/components/Nav/categoryData.js
@@ -0,0 +1,128 @@
+export const CATEGORY_DATA_LIST_1 = [
+  {
+    id: 1,
+    value: '캐릭터 드로잉',
+  },
+  {
+    id: 2,
+    value: '일러스트',
+  },
+  {
+    id: 3,
+    value: '컨셉아트',
+  },
+  {
+    id: 4,
+    value: '웹툰',
+  },
+  {
+    id: 5,
+    value: '캘리그라피',
+  },
+];
+export const CATEGORY_DATA_LIST_2 = [
+  {
+    id: 1,
+    value: '대바늘 뜨개',
+  },
+  {
+    id: 2,
+    value: '도자기 공예',
+  },
+  {
+    id: 3,
+    value: '실 공예',
+  },
+  {
+    id: 4,
+    value: '라탄 공예',
+  },
+  {
+    id: 5,
+    value: '가죽 공예',
+  },
+];
+
+export const CATEGORY_DATA_LIST_3 = [
+  {
+    id: 1,
+    value: '요가',
+  },
+  {
+    id: 2,
+    value: '필라테스',
+  },
+  {
+    id: 3,
+    value: '피트니스',
+  },
+  {
+    id: 4,
+    value: '러닝',
+  },
+  {
+    id: 5,
+    value: '발레',
+  },
+  {
+    id: 6,
+    value: '라이딩',
+  },
+];
+
+export const CATEGORY_DATA_LIST_4 = [
+  {
+    id: 1,
+    value: '주식',
+  },
+  {
+    id: 2,
+    value: '부동산',
+  },
+];
+
+export const CATEGORY_DATA_LIST_5 = [
+  {
+    id: 1,
+    value: 'Git',
+  },
+  {
+    id: 2,
+    value: '파이썬',
+  },
+  {
+    id: 3,
+    value: 'Django',
+  },
+  {
+    id: 4,
+    value: 'Node.js',
+  },
+  {
+    id: 5,
+    value: 'JavaScript',
+  },
+];
+
+export const CATEGORY_DATA_LIST_6 = [
+  {
+    id: 1,
+    value: '한식',
+  },
+  {
+    id: 2,
+    value: '양식',
+  },
+  {
+    id: 3,
+    value: '일식',
+  },
+  {
+    id: 4,
+    value: '중식',
+  },
+  {
+    id: 5,
+    value: '베이킹',
+  },
+];
