0253a2c25ffe1f89924c1e310f1a45cc4e5f8347
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2021-12-23 13:52:46 +0900

    Add : 좋아요 완료

--- src/config.js
+++ src/config.js
@@ -1,6 +1,11 @@
-const BASE_URL = 'http://10.58.5.19:8000';
+const BASE_URL = 'http://10.58.6.160:8000';
 
 export const API = {
   LOGIN: `${BASE_URL}/users/kakaosignin`,
   CLASS_COURSES: `${BASE_URL}/creators`,
+  COMMENT: `${BASE_URL}/products/comments`,
+  MYPAGE: `${BASE_URL}/users/mypage`,
+  DETAIL_PAGE: `${BASE_URL}/products/detail`,
+  LIKE: `${BASE_URL}/products/like`,
+  PRODUCTS: `${BASE_URL}/products`,
 };
